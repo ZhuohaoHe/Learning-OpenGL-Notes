ee14d4b83367d9bd16e86ffe1cfed9d1cd1ffe35
ZhuohaoHe <zhuohao.he@outlook.com>
2024-01-14 20:24:21 -0500

    T1-4: Texture

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <learnopengl/stb_image.h>
 #include <learnopengl/shader.hpp>
 
 #include <iostream>
 
-void processInput(GLFWwindow *window); 
+void processInput(GLFWwindow *window, float *mixValue); 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 // settings
@@ -16,18 +17,17 @@ const unsigned int SCR_HEIGHT = 600;
 // OpenGL only processes 3D normalized device coordinates, 
 // which means they are in between -1.0 and 1.0 in all 3 axes
 
-float vertices1[] = {
-    // first triangle position, color 
-    -0.9f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f, // left 
-    -0.0f, 0.5f, 0.0f,     0.0f, 1.0f, 0.0f, // right
-    0.45f, 0.5f, 0.0f,     0.0f, 0.0f, 1.0f, // top 
-}; 
+float vertices[] = {
+    // positions          // colors           // texture coords
+    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+};
 
-float vertices2[] = {
-     // second triangle
-    0.0f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f,// left
-    0.9f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f,// right
-    -0.45f, 0.5f, 0.0f,      0.0f, 0.0f, 1.0f// top 
+unsigned int indices[] = {  
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
 };
 
 int main(){
@@ -67,45 +67,113 @@ int main(){
 
     // set up buffer for data
 
-    unsigned int VAOs[2], VBOs[2];
-    glGenVertexArrays(2, VAOs);
-    glGenBuffers(2, VBOs);
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-    glBindVertexArray(VAOs[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    glBindVertexArray(VAO);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);  
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
-    glBindVertexArray(VAOs[1]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+    // load and create texture
+    // texture 1
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // set wrapping 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // set filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+    // load image
+    int width, height, nrChannels;
+    
+    unsigned char *data = stbi_load("resources/textures/container.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D,
+                        0, 
+                        GL_RGB, 
+                        width, 
+                        height, 
+                        0, 
+                        GL_RGB, 
+                        GL_UNSIGNED_BYTE, 
+                        data);
 
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Filed to load texture." << std::endl;
+    }
+    stbi_image_free(data);
+
+    // texture 2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // set wrapping
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    stbi_set_flip_vertically_on_load(true);
+    data = stbi_load("resources/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D,
+                        0, 
+                        GL_RGB, 
+                        width, 
+                        height, 
+                        0, 
+                        GL_RGBA, 
+                        GL_UNSIGNED_BYTE, 
+                        data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    ourShader.use();
+    std::string texture1Name = "texture1";
+    std::string texture2Name = "texture2";
+    std::string mixValueName = "mixValue";
+    // why here we use 0 and 1 instead of GL_TEXTURE0 and GL_TEXTURE1?
+    ourShader.setInt(texture1Name, 0);
+    ourShader.setInt(texture2Name, 1);    
+
+    float mixValue = 0.2f;
     // render loop
     while(!glfwWindowShouldClose(window)){
         // input
-        processInput(window);
+        processInput(window, &mixValue);
 
         // rendering
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glActiveTexture(GL_TEXTURE0); // activate the texture unit first before binding texture
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1); // activate the texture unit first before binding texture
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
         ourShader.use();
 
-        // render triangle
-        glBindVertexArray(VAOs[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        ourShader.setFloat(mixValueName, mixValue);
 
-        glBindVertexArray(VAOs[1]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 
         // swap buffers ( from back to front screen)
         glfwSwapBuffers(window);
@@ -113,8 +181,9 @@ int main(){
         glfwPollEvents();
     }
     
-    glDeleteVertexArrays(2, VAOs);
-    glDeleteBuffers(2, VBOs);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
@@ -123,9 +192,23 @@ int main(){
 
 // handling user input for a given window 
 // query GLFW whether relevant keys are pressed/released this frame and react accordingly
-void processInput(GLFWwindow *window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+void processInput(GLFWwindow *window, float *mixValue) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        *mixValue += 0.01f;
+        if(*mixValue >= 1.0f) {
+            *mixValue = 1.0f;
+        }
+    }
+    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        *mixValue -= 0.01f;
+        if(*mixValue <= 0.0f) {
+            *mixValue = 0.0f;
+        }
+    }
 }
 
 // this callback function executes whenever the window size changed 
