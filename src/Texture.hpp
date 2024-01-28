b90fe26f397752ca4fa9edd32b46987381d9b0e8
ZhuohaoHe <zhuohao.he@outlook.com>
2024-01-28 11:22:11 -0500

    update all

---
+++ src/Texture.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <string>
+
+class Texture {
+    private:
+        GLuint m_RendererID;
+        const std::string m_FilePath;
+        unsigned char* m_LocalBuffer;
+        int m_Width, m_Height, m_BPP;
+
+    public:
+        Texture(const std::string& path);
+        ~Texture();
+
+        void Bind(const unsigned int slot = 0) const;
+        void Unbind() const;
+
+        inline int GetWidth() const { return m_Width; }
+        inline int GetHeight() const { return m_Height; }
+
+};
---
+++ src/main.cpp
@@ -0,0 +1,388 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "vendor/stb_image.h"
+
+#include "VertexBuffer.hpp"
+#include "IndexBuffer.hpp"
+#include "VertexArray.hpp"
+#include "Shader.hpp"
+#include "Camera.hpp"
+#include "GLDebug.hpp"
+#include "Renderer.hpp"
+#include "Texture.hpp"
+
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <vector>
+
+void processInput(GLFWwindow *window); 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// set up triangle vertices data
+// OpenGL only processes 3D normalized device coordinates, 
+// which means they are in between -1.0 and 1.0 in all 3 axes
+
+float vertices[] = {
+        // 0-2: posistion,  3-4: texture, 5-7: normal vector
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.0f,  0.0f, -1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.0f,  0.0f, -1.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, -1.0f,  0.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f, -1.0f,  0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f,  0.0f
+    };
+
+unsigned int indices[] = {
+    // Front face
+    0, 1, 2, 3, 4, 5,
+    // Right face
+    6, 7, 8, 9, 10, 11,
+    // Back face
+    12, 13, 14, 15, 16, 17,
+    // Left face
+    18, 19, 20, 21, 22, 23,
+    // Bottom face
+    24, 25, 26, 27, 28, 29,
+    // Top face
+    30, 31, 32, 33, 34, 35
+};
+
+// GLfloat vertices[] = {
+//     // Positions          // Texture Coords
+//     // Front face
+//     -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // Vertex 0
+//      0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // Vertex 1
+//      0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // Vertex 2
+//     -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // Vertex 3
+    
+//     // Back face
+//     -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Vertex 4
+//      0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // Vertex 5
+//      0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // Vertex 6
+//     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // Vertex 7
+// };
+
+// unsigned int indices[] = {
+//     // Front face
+//     0, 1, 2, 2, 3, 0,
+//     // Right face
+//     1, 5, 6, 6, 2, 1,
+//     // Back face
+//     7, 6, 5, 5, 4, 7,
+//     // Left face
+//     4, 0, 3, 3, 7, 4,
+//     // Bottom face
+//     4, 5, 1, 1, 0, 4,
+//     // Top face
+//     3, 2, 6, 6, 7, 3
+// };
+
+
+glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)  
+};
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+float mixValue = 0.2f;
+
+bool firstMouse = true;
+
+float lastX = 400.0f, lastY = 300.0f;
+
+// camera
+Camera camera;
+
+// lighting
+glm::vec3 lightPos(3.6f, 3.0f, 6.0f);
+
+int main(){
+    glfwInit();
+
+    // set OpenGL version to 4.1
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    
+    // set OpenGL profile to core-profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // set OpenGL forward compatibility to true, 
+    // which means application uses only the modern features of OpenGL
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for macOS
+    
+    // creating window
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL){
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // window -> context
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+    glEnable(GL_DEPTH_TEST);
+
+
+/*  -----   Shader  -----   */
+    Shader basicShader("res/shaders/BasicShader.shader");
+    Shader lightShader("res/shaders/LightShader.shader");
+/*  -----   -----  -----   */
+
+/*  -----   Blending  -----   */
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+/*  -----   -----  -----   */
+
+
+/*          ****    ****    ****        */
+/*  -----   data & buffer & array ----- */
+/*          ****    ****    ****        */
+
+    VertexArray cubeVa;
+
+    VertexBuffer cubeVb(vertices, sizeof(vertices));
+
+    VertexBufferLayout cubeLayout;
+
+    cubeLayout.Push<float>(3);
+    cubeLayout.Push<float>(2);
+    cubeLayout.Push<float>(3);
+
+    cubeVa.AddBuffer(cubeVb, cubeLayout);
+
+    IndexBuffer cubeIb(indices, sizeof(indices) / sizeof(unsigned int));
+
+    // light
+    VertexArray lightVa;
+
+    VertexBufferLayout lightLayout;
+
+    lightLayout.Push<float>(3);
+
+    lightVa.AddBuffer(cubeVb, lightLayout); // share same cubeVb, because light is a cube too
+
+    cubeIb.UnBind();
+
+/*  -----    ------    -----    */
+
+
+/*  -----   Texture  -----   */
+    
+    Texture texture1("res/textures/container.jpg");
+    Texture texture2("res/textures/awesomeface.png");
+
+    // tell OpenGL for each sampler to which texture unit it belongs to (only has to be done once)
+    texture1.Bind(0);
+    texture2.Bind(1);
+    basicShader.Use();
+    basicShader.setInt("texture1", 0);
+    basicShader.setInt("texture2", 1);
+    basicShader.UnUse(); 
+
+/*  -----   -----   -----   -----   */
+
+    glm::vec3 objectColor(0.80f, 0.52f, 0.25f);
+    glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
+
+    float drop_speed = 0.01f;
+    float bottom_y = -3.0f;
+    float top_y = 3.0f;
+    // render loop
+    Renderer renderer;
+    while(!glfwWindowShouldClose(window)){
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        processInput(window);
+
+        // rendering
+        renderer.Clear();
+
+        // --- 2D ---
+        glm::mat4 trans = glm::mat4(1.0f);
+
+        // --- 3D ---
+        glm::mat4 projection = glm::perspective(glm::radians(FOV), (float)SCR_WIDTH / (float)SCR_HEIGHT, 1.0f, 100.f);
+        glm::mat4 view = camera.getViewMatrix();
+        glm::mat4 model = glm::mat4(1.0f);
+
+        /*  -----   draw light cube -----   */
+        lightShader.Use();
+        lightShader.setMat4("projection", projection);
+        lightShader.setMat4("view", view);
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+        lightShader.setMat4("model", model);
+        
+        renderer.Draw(lightVa, cubeIb);
+
+        lightShader.UnUse();
+        /*  -----   -----   -----   -----   */
+
+        /*  -----   draw cubes -----   */
+        basicShader.Use();
+        basicShader.setFloat("mixValue", mixValue);
+        
+        basicShader.setMat4("projection", projection);
+        basicShader.setMat4("view", view);
+        basicShader.setMat4("model", model);
+        basicShader.setMat4("trans", trans); 
+
+        basicShader.setVec3("objectColor", objectColor);
+        basicShader.setVec3("lightColor", lightColor);
+        basicShader.setVec3("lightPos", lightPos);
+        basicShader.setVec3("viewPos", camera.Position);
+
+        for(unsigned int i = 0; i < 10; i++){
+            glm::mat4 model = glm::mat4(1.0f);
+            cubePositions[i].y -= drop_speed;
+            if (cubePositions[i].y < bottom_y) {
+                cubePositions[i].y = top_y;
+            }
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i; 
+            model = glm::rotate(model, glm::radians(angle) + (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+            basicShader.setMat4("model", model);
+
+            renderer.Draw(cubeVa, cubeIb);
+        }
+
+        basicShader.UnUse();
+        /*  -----   -----   -----   -----   */
+
+        // swap buffers ( from back to front screen)
+        glfwSwapBuffers(window);
+        // poll IO events
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    return 0;
+
+}
+
+// handling user input for a given window 
+// query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        mixValue += 0.01f;
+        if(mixValue >= 1.0f) {
+            mixValue = 1.0f;
+        }
+    }
+    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        mixValue -= 0.01f;
+        if(mixValue <= 0.0f) {
+            mixValue = 0.0f;
+        }
+    }
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        camera.processKeyboard(FORWARD, deltaTime);
+    }
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        camera.processKeyboard(BACKWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        camera.processKeyboard(RIGHT, deltaTime);
+    }
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        camera.processKeyboard(LEFT, deltaTime);
+    }
+}
+
+// this callback function executes whenever the window size changed 
+//  adjust the OpenGL viewport to match the new window size
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    // make sure the viewport matches the new window dimensions
+    glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn) {
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = ypos - lastY;
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.processMouse(xoffset, yoffset);
+}
